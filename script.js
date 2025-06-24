c97b9559046dad2425cca884c61da0c0d957af62
Robinson Lal <robinsonlal1997@gmail.com>
2025-06-24 23:07:07 +0530

    Added functionality with javascript

---
+++ script.js
@@ -0,0 +1,78 @@
+
+const boxes = document.querySelectorAll('.box')
+const restartBtn = document.querySelector('.reset-btn')
+const newGameBtn = document.querySelector('.new-btn')
+const msgContainer = document.querySelector('.msg-container')
+const msg = document.querySelector('.msg')
+
+let turnO = true;
+
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add('hide');
+}
+
+
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        if (turnO) {
+            box.innerText = "O"
+            turnO = false;
+        } else {
+            box.innerText = "X"
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkWinner();
+    })
+})
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congartulations the winner is player ${winner}`
+    msgContainer.classList.remove('hide')
+    disableBoxes();
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "", pos2Val != "", pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("winner", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
+    }
+}
+
+newGameBtn.addEventListener('click', resetGame);
+restartBtn.addEventListener('click', resetGame)
